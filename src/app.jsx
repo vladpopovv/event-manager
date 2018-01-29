c50710d942c73eb8007003bd6139319b294036cc
vpopov <vllad1108@gmail.com>
2018-01-29 16:43:26 +0600

    Add redux store

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import createStore from './store/createStore';
 import MainPage from './containers/MainPage';
 import LoginPage from './containers/LoginPage';
 import SingUpPage from './containers/SingUpPage';
 import './style/main.less';
 
+const store = createStore();
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/singup" component={SingUpPage} />
-    </Switch>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/singup" component={SingUpPage} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('app'),
 );
